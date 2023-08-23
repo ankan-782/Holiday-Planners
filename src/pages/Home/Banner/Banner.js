05f12db421ea5b705750f0ffb4a41b07d58086ea
Avijit Roy <royavijit782@gmail.com>
2023-08-23 21:00:03 +0600

    hero section image has been added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import appleStoreLogo from "../../../../src/images/hero/apple-store-logo.png";
 import googlePlayStoreLogo from "../../../../src/images/hero/google-play-store-logo.png";
+import heroPhoto from "../../../images/Banner/travel.svg";
 import './Banner.css';
 
 const Banner = () => {
+    const divRef = useRef(null);
+    const [divHeight, setDivHeight] = useState(0);
+
+    useEffect(() => {
+        const observer = new ResizeObserver(entries => {
+            for (let entry of entries) {
+                const height = entry.target.clientHeight;
+                setDivHeight(height + 8);
+                console.log('Div height:', divHeight);
+            }
+        });
+
+        if (divRef.current) {
+            observer.observe(divRef.current);
+        }
+
+        // return () => {
+        //     if (divRef.current) {
+        //         observer.unobserve(divRef.current);
+        //     }
+        // };
+    }, [divHeight]);
+
     return (
         <div className="banner-bg">
-            <div className="container px-lg-0 px-4">
-                <div class="box_parent">
-                    <div className="row align-items-center banner-content">
-                        <div className="col-12 col-lg-6">
-                            <div className="p-5">
-                                <h1 className="banner-title mb-5 fw-bold">Discover, plan, and embark on unforgettable journeys with Holiday Planner.</h1>
-                                <p className="lh-base fw-bold w-75">Your personalized guide to seamless travel experiences and cherished memories.</p>
-                                <div className="d-flex mt-5">
-                                    <img style={{ width: "150px", height: "50px" }} src={appleStoreLogo} alt="apple store logo" className="me-3" />
-                                    <img style={{ width: "150px", height: "50px" }} src={googlePlayStoreLogo} alt="play store logo" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-12 col-lg-6">
-
-                        </div>
-                    </div>
-
+            <div className="container px-lg-0 px-4 position-relative">
+                <div style={{ height: divHeight }} className="banner-content">
+                    <div className="banner-content-bg"></div>
                     <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
                         <defs>
                             <filter id="flt_tag">
@@ -33,7 +42,23 @@ const Banner = () => {
                             </filter>
                         </defs>
                     </svg>
-
+                </div>
+                <div ref={divRef} id="myDiv" style={{ zIndex: 999 }} className="row align-items-center position-absolute top-0">
+                    <div className="col-12 col-lg-6">
+                        <div className="p-5">
+                            <h1 className="banner-title mb-5 fw-bold">Discover, plan, and embark on unforgettable journeys with Holiday Planner.</h1>
+                            <p className="lh-base fw-bold w-75">Your personalized guide to seamless travel experiences and cherished memories.</p>
+                            <div className="d-flex mt-5">
+                                <img style={{ width: "150px", height: "50px" }} src={appleStoreLogo} alt="apple store logo" className="me-3" />
+                                <img style={{ width: "150px", height: "50px" }} src={googlePlayStoreLogo} alt="play store logo" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                        <div className="">
+                            <img src={heroPhoto} alt="hero section" className="img-fluid" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -41,45 +66,3 @@ const Banner = () => {
 };
 
 export default Banner;
-
-{/* <div id="carouselExampleCaptions" className="carousel slide carousel-fade" data-bs-ride="carousel">
-    <div className="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div className="carousel-inner">
-        <div className="carousel-item active">
-            <div className='slide1 min-vh-100'>
-                <div className="carousel-caption d-none d-md-block">
-                    <h1 className="mb-5">Enjoy The Travel with <br /> <span className="banner-brand m-0">Holiday Planners</span></h1>
-                    <h5 className="lh-base w-50 mx-auto line">We Provide best holiday planning to our customers so that they can travel peacefully and they can utilize their time perfectly.</h5>
-                </div>
-            </div>
-        </div>
-        <div className="carousel-item">
-            <div className='slide2 min-vh-100'>
-                <div className="carousel-caption d-none d-md-block">
-                    <h1 className="mb-5">Life is Short and <br /> <span className="banner-brand">The World is Wide</span></h1>
-                    <h5 className="lh-base w-50 mx-auto line">If you want to travel, but you’re not sure where to start, you just need to take the first step. Research places you’d like to go</h5>
-                </div>
-            </div>
-        </div>
-        <div className="carousel-item">
-            <div className='slide3 min-vh-100'>
-                <div className="carousel-caption d-none d-md-block">
-                    <h1 className="mb-5">Get ready to Travel <br /> <span className="banner-brand">The World</span></h1>
-                    <h5 className="lh-base w-50 mx-auto line">Twenty years from now you will be more disappointed by the things you didn’t do than by the ones you did do. – Mark Twain</h5>
-                </div>
-            </div>
-        </div>
-    </div>
-    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-    </button>
-    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-    </button>
-</div> */}
--- src/pages/Home/CallToAction/CallToAction.js
+++ src/pages/Home/CallToAction/CallToAction.js
@@ -5,12 +5,12 @@ import './CallToAction.css';
 export default function CallToAction() {
     return (
         <div className="callToAction-bg">
-            <div className="container px-lg-0 px-4 py-5">
+            <div className="container px-lg-0 px-4">
                 <div className="callToAction-content">
                     <h2 className="banner-title mb-4 fw-bold fs-1">Download the Holiday Planners app now!</h2>
                     <p className="lh-base fw-bold">Be the first one to explore one of the best travel application</p>
-                    <div className="d-flex justify-content-center mt-4">
-                        <img style={{ width: "150px", height: "50px" }} src={appleStoreLogo} alt="apple store logo" className="me-3" />
+                    <div className="d-flex flex-column flex-sm-row justify-content-center align-items-center mt-4">
+                        <img style={{ width: "150px", height: "50px" }} src={appleStoreLogo} alt="apple store logo" className="me-sm-3 mb-2 mb-sm-0" />
                         <img style={{ width: "150px", height: "50px" }} src={googlePlayStoreLogo} alt="play store logo" />
                     </div>
                 </div>
